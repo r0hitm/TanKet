83b16e823e6f1529de363fd99a476d7638f3c146
Rohit M <63762981+r0hitm@users.noreply.github.com>
2021-10-23 01:28:04 +0530

    Added Missile and Enemy table, along with spawnEnemies function.
    Also added a function to display FPS on top left corner.

--- main.lua
+++ main.lua
@@ -1,11 +1,19 @@
 --[[
     TanKet
-    - A Tower Defense Survival Game.
+    - A very minimal Tower Defense Survival Game.
     - Player has to survive as long as possible from a hoard of attackers.
 
     Author: Rohit Mehta
 
     This is my Final Project for the CS50x 2021 Course by Harvard.
+]]
+
+--[[
+    In the battlefield, you are supposed to wreak havoc on the enemy using your tank.
+    But tank's treads have been damaged severly, and you cannot move, but the war is going on!
+    Hopefully, your turrets are working fine and you have good amount of ammunition still available.
+
+    Shoot out the enemies and survive as long as possible on the battlefield!
 ]]
 
 --[[
@@ -22,14 +30,24 @@ function love.load()
     ]]
     Object = require 'classic'
 
-    require 'Tower'
+    require 'Tank'
     require 'Enemy'
     require 'Missile'
+
+    math.randomseed(os.time())
 
     ----------------------------------------------------------------------------
     -- Constants:
     WINDOW_WIDTH = 1280
     WINDOW_HEIGHT = 720
+
+    TITLE_FONT = love.graphics.newFont('font/market_deco.ttf', 30)
+    SMALL_FONT = love.graphics.newFont('font/market_deco.ttf', 12)
+
+    tank = Tank(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+    enemies = {}
+    missiles = {}
+    spawnEnemies(100)
 
     love.window.setTitle('TanKet')
     --[[
@@ -41,11 +59,6 @@ function love.load()
         fullscreen = false,
         resizable = false
     })
-
-    player = Tower(WINDOW_WIDTH / 2 - 30, WINDOW_HEIGHT / 2 - 30) -- the central tower
-
-    -- testEnemy = Enemy(WINDOW_WIDTH, WINDOW_HEIGHT / 2, 'rtl')
-    -- testMissile = Missile(WINDOW_WIDTH / 2 - 30, WINDOW_HEIGHT / 2 - 30, 'ltr')
 
     --[[
         Gamestate is one of:
@@ -62,25 +75,57 @@ end
 ]]
 function love.update(dt)
     if Gamestate == "start" then
-        
+        --[[
+            handle events that trigger the "play" state.
+            And make the "start" screen little interactive by adding animation
+        ]]
     elseif Gamestate == "play" then
+        if love.keyboard.isDown('right') then
+            tank:turnClock()
+        elseif love.keyboard.isDown('left') then
+            tank:turnAntiClock()
+        end
 
-    end
-    if love.keyboard.isDown('w') then
-        player:moveUp()
-    elseif love.keyboard.isDown('s') then
-        player:moveDown()
-    elseif love.keyboard.isDown('d') then
-        player:moveRight()
-    elseif love.keyboard.isDown('a') then
-        player:moveLeft()
+        for _, missile in ipairs(missiles) do
+            missile:update(dt)
+        end
+
+        for _, enemy in ipairs(enemies) do
+            enemy:approach(dt, WINDOW_WIDTH / 2 - 30, WINDOW_HEIGHT / 2 - 30)
+        end
     end
 end
 
 function love.draw()
-    -- testEnemy:render()
-    player:render()
-    -- testMissile:render()
+    displayFPS()
+
+    if Gamestate == "start" then
+        --[[
+            print a big welcome message onto the screen,
+            and other messages, including lore
+        ]]
+        -- for now just print a simple message
+        love.graphics.printf(
+            'Press Enter to Start the Game',
+            TITLE_FONT,
+            0,
+            WINDOW_HEIGHT / 2 - 6,
+            WINDOW_WIDTH,
+            "center"
+        )
+
+    elseif Gamestate == "play" then
+        love.graphics.setBackgroundColor(0.5, 0.5, 0.15)
+        tank:render()
+
+        for _, enemy in ipairs(enemies) do
+            enemy:render()
+        end
+
+        for _, missile in ipairs(missiles) do
+            missile:render()
+        end
+    end
 end
 
 
@@ -90,16 +135,25 @@ end
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
-    elseif key == 'enter' then
+    elseif key == 'enter' or key == 'return' then
         if Gamestate == "start" then Gamestate = "play" end
     end
+end
+
+function love.keyreleased(key)
+    if key == 'space' then
+        table.insert(missiles, #missiles + 1, Missile(tank:getTurretPos()))
+    end
+end
+
+function love.quit()
+    print('Thanks for playing! Come back soon!')
 end
 
 --[[
     Object Object -> Boolean
     produce true if the given objects are colliding
 ]]
----@diagnostic disable-next-line: lowercase-global
 function areColliding(object1, object2)
     if not object1 and object2 then  -- either of the objects is nil
         return false
@@ -107,4 +161,35 @@ function areColliding(object1, object2)
 
     -- using AABB collision detection
 
+end
+
+--[[
+    Randomly spawn 4 enemies off the screen
+]]
+function spawnEnemies(num)
+    local i = 1
+    while i <= num do
+        table.insert(
+            enemies,
+            #enemies + 1,
+            Enemy(math.random() < .5 and math.random(-100, 0) or math.random(WINDOW_WIDTH, WINDOW_WIDTH + 100),
+                math.random(WINDOW_HEIGHT)))
+
+        table.insert(enemies,
+            #enemies + 1,
+            Enemy(math.random(WINDOW_WIDTH),
+                math.random() < .5 and math.random(-100, 0) or math.random(WINDOW_HEIGHT, WINDOW_HEIGHT + 100)))
+        i = i + 1
+    end
+end
+
+--[[
+    Renders the current FPS.
+]]
+function displayFPS()
+    -- simple FPS display across all states
+    love.graphics.setFont(SMALL_FONT)
+    love.graphics.setColor(0, 255/255, 0, 255/255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
+    love.graphics.setColor(1,1,1)
 end
