a1613e01816abfac7fd871bdc47eb0fa96ecc79a
Rohit M <63762981+r0hitm@users.noreply.github.com>
2021-10-22 23:15:51 +0530

    Restructured Enemy class. Simplified. And added a new logic to approach a particular position

--- Enemy.lua
+++ Enemy.lua
@@ -1,12 +1,13 @@
 --[[
     TanKet
-    - A Two Player Tank Battle Game.
+    - A very minimal Tank Defense Survival Game.
+    - Player has to survive as long as possible from a hoard of attackers.
 
-    -- Enemy Class --
+    --- Enemy Class ---
 
     Author: Rohit Mehta
 
-    Represents the enemies that attack from each edge towards the center.
+    Represents the generic Enemy in the game.
 ]]
 
 Enemy = Object:extend()
@@ -16,35 +17,47 @@ local HEIGHT = 20
 
 local SPEED = 1
 
---[[
-    Move is one of
-    - 'ltr'
-    - 'rtl'
-    - 'ttb'
-    - 'btt'
-
-    interp, moving left-to-right, right-to-left, top-to-bottom or, botom-to-top
-]]
-
 -- Enemy:new()
 -- Integer Integer Move -> Enemy
 -- creates an enemy at x,y
-function Enemy:new(x, y, move)
+function Enemy:new(x, y)
   self.x = x or 0
   self.y = y or 0
-
-  self.move = move or 'rtl'
 end
 
-function Enemy:update()
-    if self.move == 'ltr' then
-        self.x = self.x + SPEED
-    elseif self.move == 'rtl' then
-        self.x = self.x - SPEED
-    elseif self.move == 'ttb' then
-        self.y = self.y + SPEED
-    elseif self.move == 'btt' then
-        self.y = self.y - SPEED
+function Enemy:getPos()
+    return self.x, self.y
+end
+
+--[[
+    Float Number Number -> nil
+    moves the enemy towards the given coordinates (x, y) by in dt
+]]
+function Enemy:approach(dt, x, y)
+    -- move along x-axis
+    if x > self.x then      -- move towards right of current position
+        self.x = self.x + SPEED * dt
+    elseif x < self.x then  -- move towards the left of current position
+        self.x = self.x - SPEED * dt
+    end
+
+    -- move along y-axis
+    if y > self.y then      -- move down from the current position
+        self.y = self.y + SPEED * dt
+    elseif y < self.y then  -- move up from the current position
+        self.y = self.y - SPEED * dt
+    end
+end
+
+--[[
+    Number Number -> Boolean
+    Produce true if the current positon is same as the given x,y
+]]
+function Enemy:atCoord(x, y)
+    if self.x == x and self.y == y then
+        return true
+    else
+        return false
     end
 end
 
